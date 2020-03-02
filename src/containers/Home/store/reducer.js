d4b1d3b26634ae2ce90cb9bf851ba9b59c64e941
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-03-02 15:19:56 +0300

    Track field hasError in the coins store

--- src/containers/Home/store/reducer.js
+++ src/containers/Home/store/reducer.js
@@ -7,6 +7,7 @@ import {
 export const initialState = {
   items: [],
   isLoading: false,
+  hasError: false,
   error: null
 };
 
@@ -15,19 +16,22 @@ export default function coinReducer(state = initialState, action) {
     case GET_COIN_DATA_REQUEST:
       return {
         ...state,
-        isLoading: true
+        isLoading: false,
+        isLoading: true,
       };
     case GET_COIN_DATA_SUCESS:
       return {
         ...state,
         items: action.payload.items,
         isLoading: false,
+        hasError: false,
         error: null
       };
     case GET_COIN_DATA_FAILURE:
       return {
         ...state,
         isLoading: false,
+        hasError: true,
         error: action.payload.error
       };
   }
